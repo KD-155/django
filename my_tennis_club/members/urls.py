0fb73c4e1ab81b6728f3e0865878e694ea06c460
Kelly Dumbrique <ksdumbrique2@student.apc.edu.ph>
2025-03-06 06:59:28 +0000

    django urls

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
-from django.urls import include, path
- 
- 
+from django.urls import path
+from . import views
+
 urlpatterns = [
-    path('', include('members.urls')),
-    path("admin/", admin.site.urls),
+    path('members/', views.members, name='members'),
 ]
